88dc5aef15653842cb63400e19d65a2c2120ea82
Mitul <mitul.panchal72@gmail.com>
2018-05-29 00:52:17 +0530

    MongoDB Application

---
+++ SampleMongoDBApplication/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SampleMongoDBApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.grid1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(170, 77);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(129, 20);
+            this.txtsearch.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // grid1
+            // 
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.Location = new System.Drawing.Point(1, 113);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(796, 335);
+            this.grid1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grid1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView grid1;
+    }
+}
+
---
+++ SampleMongoDBApplication/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Linq;
+
+namespace SampleMongoDBApplication
+{
+    public partial class Form1 : Form
+    {
+        private IMongoDatabase _database { get; }
+        MongoClient client = new MongoClient("mongodb://localhost:27017");
+       
+        public Form1()
+        {
+            InitializeComponent();                                   
+        }
+            
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IMongoDatabase db = client.GetDatabase("test");
+            var obj = db.GetCollection<BsonDocument>("test");
+            string val = txtsearch.Text;
+            var filter = Builders<BsonDocument>.Filter.Eq("status", val);
+            var result = obj.Find(filter).ToList();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Item", typeof(string));
+            dt.Columns.Add("status", typeof(string));
+            foreach (var item in result)
+            {
+                dt.Rows.Add(item["item"]);
+                dt.Rows.Add(item["status"]);
+            }
+            grid1.DataSource = dt;
+        }
+    }
+
+}
+
+//            var documents = new[]
+//{
+//    new BsonDocument
+//    {
+//        { "item", "journal" },
+//        { "status", "A" },
+//        { "size", new BsonDocument { { "h", 14 }, { "w", 21 }, { "uom", "cm" } } },
+//        { "instock", new BsonArray
+//            {
+//                new BsonDocument { { "warehouse", "A" }, { "qty", 5 } } }
+//            }
+//    },
+//    new BsonDocument
+//    {
+//        { "item", "notebook" },
+//        { "status", "A" },
+//        { "size", new BsonDocument { { "h", 8.5 }, { "w", 11 }, { "uom", "in" } } },
+//        { "instock", new BsonArray
+//            {
+//                new BsonDocument { { "warehouse", "C" }, { "qty", 5 } } }
+//            }
+//    },
+//    new BsonDocument
+//    {
+//        { "item", "paper" },
+//        { "status", "D" },
+//        { "size", new BsonDocument { { "h", 8.5 }, { "w", 11 }, { "uom", "in" } } },
+//        { "instock", new BsonArray
+//            {
+//                new BsonDocument { { "warehouse", "A" }, { "qty", 60 } } }
+//            }
+//    },
+//    new BsonDocument
+//    {
+//        { "item", "planner" },
+//        { "status", "D" },
+//        { "size", new BsonDocument { { "h", 22.85 }, { "w", 30 }, { "uom", "cm" } } },
+//        { "instock", new BsonArray
+//            {
+//                new BsonDocument { { "warehouse", "A" }, { "qty", 40 } } }
+//            }
+//    },
+//    new BsonDocument
+//    {
+//        { "item", "postcard" },
+//        { "status", "A" },
+//        { "size", new BsonDocument { { "h", 10 }, { "w", 15.25 }, { "uom", "cm" } } },
+//        { "instock", new BsonArray
+//            {
+//                new BsonDocument { { "warehouse", "B" }, { "qty", 15 } },
+//                new BsonDocument { { "warehouse", "C" }, { "qty", 35 } } }
+//            }
+//    }
+//};
+//            obj.InsertMany(documents);
+
